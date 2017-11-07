10a73c5b60b7f4ca4d78700f0eca12a313a9c465
gabrielqap <30510935+gabrielqap@users.noreply.github.com>
2017-11-07 15:14:50 -0300

    Create list0901v3.cpp

---
+++ lists/list0901v3.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+	vector<int> data{};
+	int x{};
+	// initialized to be empty.
+	// Read integers one at a time.
+	while(cin >> x)
+	// Store eah integer int the vector.
+	data.push_back(x);
+	// Sort the vector.
+	sort(data.begin(), data.end());
+	// Print the vector, one number per line.
+		for (int element : data)
+			cout << element << endl;
+}
